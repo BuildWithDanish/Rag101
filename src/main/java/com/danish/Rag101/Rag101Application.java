aac18865c7dfef09ee96da5d9bf40b0650fafaa0
Mohammad Danish <danishsince04@gmail.com>
2026-07-16 02:27:07 +0530

    initial commit

---
+++ src/main/java/com/danish/Rag101/Rag101Application.java
@@ -0,0 +1,13 @@
+package com.danish.Rag101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rag101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rag101Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/danish/Rag101/Rag101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danish.Rag101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rag101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
